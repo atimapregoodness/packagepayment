ddcbe0a09a02af0edd1e7329cc8e39e82cbd2e28
Atimapre Goodness <atimaxgoodness@gmail.com>
2025-09-10 11:25:40 +0100

    added some features

--- src/routes/viewLink.js
+++ src/routes/viewLink.js
@@ -7,15 +7,37 @@ const Client = require("../models/client");
 // const User = require("../models/client");
 // const crypto = require("crypto");
 
-router.get("/link-info/:txId", isAdmin || isCreator, async (req, res) => {
-  const { txId } = req.params;
-  const client = await Client.findOne({ transactionId: txId }).populate(
-    "author"
-  );
+// router.get("/link-info/:txId", isAdmin || isCreator, async (req, res) => {
+//   const { txId } = req.params;
+//   const client = await Client.findOne({ transactionId: txId }).populate(
+//     "author"
+//   );
 
-  console.log(client);
+//   console.log(client);
 
-  res.render("clientDetails", { client, showLayout: false });
+//   res.render("clientDetails", { client, showLayout: false });
+// });
+
+router.get("/link-info/:transactionId", async (req, res) => {
+  try {
+    const client = await Client.findOne({
+      transactionId: req.params.transactionId,
+    });
+
+    if (!client) {
+      return res
+        .status(404)
+        .send("<p class='text-danger'>Client not found</p>");
+    }
+
+    // Render only the partial
+    res.render("partials/paymentDetails", { client });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .send("<p class='text-danger'>Server error loading details</p>");
+  }
 });
 
 module.exports = router;
