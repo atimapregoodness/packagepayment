7355e4988d81b54e09fccbe7661644c35aa8186a
Atimapre Goodness <atimaxgoodness@gmail.com>
2025-09-13 18:11:56 +0100

    restriction

--- src/middleware/isNotRestricted.js
+++ src/middleware/isNotRestricted.js
@@ -9,11 +9,29 @@
 //   next();
 // };
 
+// module.exports = function (req, res, next) {
+//   if (req.user && req.user.restricted) {
+//     return res.send(`
+//       <script>
+//         alert("Sorry, your account has been restricted by the creator. You can't perform this action.");
+//         window.location.href = "/admin/dashboard";
+//       </script>
+//     `);
+//   }
+//   next();
+// };
+
 module.exports = function (req, res, next) {
   if (req.user && req.user.restricted) {
     return res.send(`
       <script>
-        alert("Sorry, your account has been restricted by the creator. You can't perform this action.");
+        const warningMessage = "🚫 RESTRICTION 🚫\\n\\n" +
+          "Your account has been RESTRICTED by the Creator.\\n" +
+          "You can no longer perform this action.\\n\\n" +
+          "Please contact the Creator for assistance.\\n\\n" +
+          "Press OK to return to your dashboard.";
+
+        alert(warningMessage);
         window.location.href = "/admin/dashboard";
       </script>
     `);
