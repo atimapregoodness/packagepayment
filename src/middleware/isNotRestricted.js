dd33b544d2a7d38395a0601182ccc4f9b492dc9c
Atimapre Goodness <atimaxgoodness@gmail.com>
2025-09-13 18:08:25 +0100

    made changes

--- src/middleware/isNotRestricted.js
+++ src/middleware/isNotRestricted.js
@@ -1,10 +1,22 @@
+// module.exports = function (req, res, next) {
+//   if (req.user && req.user.restricted) {
+//     req.flash(
+//       "error",
+//       "Sorry your account has been restricted by the creator, you can't perform this action"
+//     );
+//     return res.redirect("/admin/dashboard");
+//   }
+//   next();
+// };
+
 module.exports = function (req, res, next) {
   if (req.user && req.user.restricted) {
-    req.flash(
-      "error",
-      "Sorry your account has been restricted by the creator, you can't perform this action"
-    );
-    return res.redirect("/admin/dashboard");
+    return res.send(`
+      <script>
+        alert("Sorry, your account has been restricted by the creator. You can't perform this action.");
+        window.location.href = "/admin/dashboard";
+      </script>
+    `);
   }
   next();
 };
